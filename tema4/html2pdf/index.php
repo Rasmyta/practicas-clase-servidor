b0e0139385af378207608e8f52d714b8d5cdd5fd
Javier GB <jjavierguillen@gmail.com>
2020-10-22 20:58:32 +0200

    Prueba solucionar pdf en Heroku

--- tema4/html2pdf/index.php
+++ tema4/html2pdf/index.php
@@ -80,8 +80,7 @@ try {
     
     $html2pdf = new Html2Pdf('P', 'A4', 'fr', true, 'UTF-8', 0);
     $html2pdf->writeHTML($content);
-    //$html2pdf->output('factura.pdf');
-    echo "No va";
+    $html2pdf->output('factura.pdf');
 } catch (Html2PdfException $e) {
     $html2pdf->clean();
 
